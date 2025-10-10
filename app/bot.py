eb9c9435777d80cb17b3611d45bbe502fb34efcd
Shurik <sanya.kucheryavvyh@bk.ru>
2025-10-10 11:32:09 +0300

    Поместил все команды в два списка. Реализовал функцию для обработки и регистрации существующих команд: main() соединил два списка с командами, тем самым сократив объём кода в блоках try/except

--- app/bot.py
+++ app/bot.py
@@ -1,18 +1,30 @@
-from telegram.ext import Application, ApplicationBuilder, CommandHandler
+from telegram import Update
+from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 from commands import CommandsMyBot
 from dotenv import load_dotenv
 import logging
 import os
 from pc_commands import PcCommandsBot
 
+
 # Загрузка переменных окружения
 load_dotenv()
 TOKEN_MY_BOT = os.getenv("TOKEN_MY_BOT")
 
+logging.basicConfig(
+    filename = 'result_work_bot.log',
+    filemode='a',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    encoding='utf-8'
+)
+
 pc_commands_bot = PcCommandsBot()
 bot_commands = CommandsMyBot()
 bot_app = Application.builder().token(TOKEN_MY_BOT).build()
+
 print('Начало запуска скрипта')
+logging.debug('Начало запуска бота')
 
 list_commands = [
     CommandHandler('start', bot_commands.start),
@@ -29,11 +41,25 @@ list_pc_commands = [
     CommandHandler('disk', pc_commands_bot.disk)
 ]
 
-if __name__ == "__main__":
-    for command in list_commands:
-        bot_app.add_handler(command)
 
-    for handler in list_pc_commands:
-        bot_app.add_handler(handler)
+async def command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Команда получена')
 
+
+def main():
+    bot_app = Application.builder().token(TOKEN_MY_BOT).build()
+    all_commands = list_commands + list_pc_commands
+
+    for command in all_commands:
+        try:
+            bot_app.add_handler(command)
+            logging.info(f'Команда {command} - успешно добавлена!')
+        except Exception as error:
+            logging.error(f"Ошибка с {command}: {error}")
+
+    # Запускаем бота
     bot_app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
