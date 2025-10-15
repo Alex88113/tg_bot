664aee4fd6ac05aed16794f97c3ec6332d006fcb
Shurik <sanya.kucheryavvkh@bk.ru>
2025-10-15 21:04:19 +0300

    Сделал более информативное инфорационное сообщение об дисках в методе disk()

--- app/pc_commands.py
+++ app/pc_commands.py
@@ -50,9 +50,9 @@ class PcCommandsBot(PcCommands):
         for partition in partitions:
             try:
                 usage = psutil.disk_usage(partition.mountpoint)
-                info_disk += f"""
-{partition.mountpoint} ({partition.fstype}\n
-{usage.total // (1024 ** 3)} gb\n
+                info_disk += f"""Раздел диска: {partition.mountpoint}
+Тип файловой системы: ({partition.fstype}\n
+Всего: {usage.total // (1024 ** 3)} gb\n
 "Использовано: {usage.used // (1024 ** 3)} gb ({usage.percent}%\n
 Свободно: {usage.free // (1024 ** 3)} gb\n"""
            
