fbea7be6f21b6ecf3915cab73aa2f6089b7f3d09
Shurik <sanya.kucheryavvyh@bk.ru>
2025-10-19 14:40:10 +0300

    Убрад повторяющиеся метод, и сделал более читаемый резульат в методе disk()

--- app/pc_commands.py
+++ app/pc_commands.py
@@ -49,38 +49,16 @@ class PcCommandsBot(PcCommands):
          await update.message.reply_text('-' * 80)
 
     async def disk(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-<<<<<<< HEAD
         info_about_disk = psutil.disk_partitions()
         for data_disk in info_about_disk:
             used = psutil.disk_usage('/')
-            all_data_disk = f"""
-Диск: {data_disk.device}
-Точка монтирования: {data_disk.mountpoint}
-Файловая система: {data_disk.fstype}
-Общий объём диска {data_disk.device}: {used.total / (1024 ** 3):.2f}"""
+            all_data_disk = f"""\n
+Диск: {data_disk.device}\n
+Точка монтирования: {data_disk.mountpoint}\n
+Файловая система: {data_disk.fstype}\n
+Общий объём диска {data_disk.device}: {used.total / (1024 ** 3):.2f}\n"""
             await update.message.reply_text(all_data_disk)
-    
-=======
-    partitions = psutil.disk_partitions(all=False)
-    info_disk = '💾 Информация о дисках:\n\n'
-    for partition in partitions:
-        try:
-            usage = psutil.disk_usage(partition.mountpoint)
-            info_disk = f"""📁 Раздел диска: {partition.mountpoint}
-Тип файловой системы: {partition.fstype}
-Всего: {usage.total // (1024 ** 3)} GB
-Использовано: {usage.used // (1024 ** 3)} GB ({usage.percent}%)
-Свободно: {usage.free // (1024 ** 3)} GB\n\n"""
-        except Exception as error:
-           return  f"❌ Ошибка: {error}\n\n"
-    await update.message.reply_text(info_disk)
 
-    async def users_system(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        users = psutil.users()
-        for user in users:
-            await update.message.reply_text(f"Текущий пользователь системы: {user}")
-
->>>>>>> e9c34ca5d487314cce7b6e90d96a92012cde2415
     async def access_memory(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         virtual_memory = psutil.virtual_memory()
         used_memory = (virtual_memory.used / virtual_memory.total) * 100
